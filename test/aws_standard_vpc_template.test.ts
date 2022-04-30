934d22c4eb50e039f466add83750387b5535ebe7
Yang Liu <yangliu@Yangs-MBP.fios-router.home>
2022-04-29 23:18:19 -0400

    Initial commit

---
+++ test/aws_standard_vpc_template.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsStandardVpcTemplate from '../lib/aws_standard_vpc_template-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws_standard_vpc_template-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsStandardVpcTemplate.AwsStandardVpcTemplateStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
